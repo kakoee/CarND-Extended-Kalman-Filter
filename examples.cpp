f790696996cccde8d8287839433426ff39573618
kakoee <32944821+kakoee@users.noreply.github.com>
2018-05-08 10:57:40 -0700

    update example

--- examples.cpp
+++ examples.cpp
@@ -271,3 +271,149 @@ MatrixXd CalculateJacobian(const VectorXd& x_state) {
 	return Hj;
 }
 
+
+int main() {
+	/*
+	 * Compute RMSE
+	 */
+	vector<VectorXd> estimations;
+	vector<VectorXd> ground_truth;
+
+	//the input list of estimations
+	VectorXd e(4);
+	e << 1, 1, 0.2, 0.1;
+	estimations.push_back(e);
+	e << 2, 2, 0.3, 0.2;
+	estimations.push_back(e);
+	e << 3, 3, 0.4, 0.3;
+	estimations.push_back(e);
+
+	//the corresponding list of ground truth values
+	VectorXd g(4);
+	g << 1.1, 1.1, 0.3, 0.2;
+	ground_truth.push_back(g);
+	g << 2.1, 2.1, 0.4, 0.3;
+	ground_truth.push_back(g);
+	g << 3.1, 3.1, 0.5, 0.4;
+	ground_truth.push_back(g);
+
+	//call the CalculateRMSE and print out the result
+	cout << CalculateRMSE(estimations, ground_truth) << endl;
+
+
+	return 0;
+}
+
+VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
+		const vector<VectorXd> &ground_truth){
+
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+    // TODO: YOUR CODE HERE
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	// ... your code here
+	if(estimations.size()==0 || (estimations.size()!=ground_truth.size()))
+	return rmse;
+
+	//accumulate squared residuals
+	VectorXd res(4);
+	for(int i=0; i < estimations.size(); ++i){
+        // ... your code here
+        VectorXd item_ext = estimations[i];
+        VectorXd item_real = ground_truth[i];
+        float resitem=0;
+        for(int j=0;j<item_ext.size();j++){
+            float sub = item_ext(j) - item_real(j);            
+            rmse(j) = rmse(j) + sub*sub;
+        }
+	}
+
+	//calculate the mean
+	// ... your code here
+	rmse << rmse/estimations.size();//(0)/4,rmse(1)/4,rmse(2)/4,rmse(3)/4 ;
+	
+	//calculate the squared root
+	// ... your code here
+	rmse << rmse.array().sqrt();//sqrt(rmse(0)), sqrt(rmse(1)), sqrt(rmse(2)),sqrt(rmse(3));  
+
+	//return the result
+	return rmse;
+}
+
+
+VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
+		const vector<VectorXd> &ground_truth);
+
+int main() {
+	/*
+	 * Compute RMSE
+	 */
+	vector<VectorXd> estimations;
+	vector<VectorXd> ground_truth;
+
+	//the input list of estimations
+	VectorXd e(4);
+	e << 1, 1, 0.2, 0.1;
+	estimations.push_back(e);
+	e << 2, 2, 0.3, 0.2;
+	estimations.push_back(e);
+	e << 3, 3, 0.4, 0.3;
+	estimations.push_back(e);
+
+	//the corresponding list of ground truth values
+	VectorXd g(4);
+	g << 1.1, 1.1, 0.3, 0.2;
+	ground_truth.push_back(g);
+	g << 2.1, 2.1, 0.4, 0.3;
+	ground_truth.push_back(g);
+	g << 3.1, 3.1, 0.5, 0.4;
+	ground_truth.push_back(g);
+
+	//call the CalculateRMSE and print out the result
+	cout << CalculateRMSE(estimations, ground_truth) << endl;
+
+
+	return 0;
+}
+
+VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
+		const vector<VectorXd> &ground_truth){
+
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	if(estimations.size() != ground_truth.size()
+			|| estimations.size() == 0){
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+
+	//accumulate squared residuals
+	for(unsigned int i=0; i < estimations.size(); ++i){
+
+		VectorXd residual = estimations[i] - ground_truth[i];
+
+		//coefficient-wise multiplication
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+
+	//calculate the mean
+	rmse = rmse/estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
+}
+
+
+
