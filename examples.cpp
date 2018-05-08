6cafa79481e0f067e53d1a80731b7d382a10d4ae
kakoee <32944821+kakoee@users.noreply.github.com>
2018-05-07 18:22:39 -0700

    update

--- examples.cpp
+++ examples.cpp
@@ -118,26 +118,27 @@ void filter(VectorXd &x, MatrixXd &P) {
 		//YOUR CODE HERE
 		
 		// KF Measurement update step
-		 // first calculate kalman gain KG : kg = E(est) / (E(est) + E (meas))
-		// H is to convert the dimention of matrix to match LHS. S.inverse is the same as 
-		// division by S. P is Error estimated, and R is the meas error.
-		MatrixXd P1 = P * H.transpose();
-		MatrixXd S = H * P * H.transpose() + R;
-		MatrixXd KG = P1 * S.inverse();
-		
-		// new state
-		// then calculate new estimate
-		// EST(new) = EST(old)  + KG(meas - EST(old)
-		MatrixXd KG_term = z - ( H * x); //  this is meas - EST(OLD); H for matrix conversion
-		x = x + (KG * KG_term);
-        P = (I - KG*H)  * P; // Error(new) = (1-KG) Error(old). I is used instead of 1 for matrix
+            // first calculate kalman gain KG : kg = E(est) / (E(est) + E (meas))
+            // H is to convert the dimention of matrix to match LHS. S.inverse is the same as 
+            // division by S. P is Error estimated, and R is the meas error.
+            MatrixXd P1 = P * H.transpose();
+            MatrixXd S = H * P * H.transpose() + R;
+            MatrixXd KG = P1 * S.inverse();
+            
+            // new state
+            // then calculate new estimate
+            // EST(new) = EST(old)  + KG(meas - EST(old)
+            MatrixXd KG_term = z - ( H * x); //  this is meas - EST(OLD); H for matrix conversion
+            x = x + (KG * KG_term);
+            P = (I - KG*H)  * P; // Error(new) = (1-KG) Error(old). I is used instead of 1 for matrix
 
+        
 		// KF Prediction step
-        // new_state = A.old_state + Bu (control matrix) + Wk (noise) . A is F here
-		x = F * x + u;
-		
-        // new_error = A. old_error . A.Tranpose ( A is F here)
-        P = F * P * F.transpose() + Q;
+            // new_state = A.old_state + Bu (control matrix) + Wk (noise) . A is F here
+            x = F * x + u;
+            
+            // new_error = A. old_error . A.Tranpose ( A is F here)
+            P = F * P * F.transpose() + Q;
 
 
 		std::cout << "x=" << std::endl <<  x << std::endl;
@@ -146,3 +147,80 @@ void filter(VectorXd &x, MatrixXd &P) {
 
 	}
 }
+
+
+#include "kalman_filter.h"
+
+KalmanFilter::KalmanFilter() {
+}
+
+KalmanFilter::~KalmanFilter() {
+}
+
+void KalmanFilter::Predict() {
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+}
+
+
+
+// Process a single measurement
+void Tracking::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
+	if (!is_initialized_) {
+		//cout << "Kalman Filter Initialization " << endl;
+
+		//set the state with the initial location and zero velocity
+		kf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+
+		previous_timestamp_ = measurement_pack.timestamp_;
+		is_initialized_ = true;
+		return;
+	}
+
+	//compute the time elapsed between the current and previous measurements
+	float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+	previous_timestamp_ = measurement_pack.timestamp_;
+	
+    // TODO: YOUR CODE HERE
+	//1. Modify the F matrix so that the time is integrated
+	kf_.F_(0,2)=dt;
+	kf_.F_(1,3)=dt;	
+	//2. Set the process covariance matrix Q
+	float dt2 = dt*dt/1;
+	float dt3 = dt2 *dt/2;
+	float dt4 = dt3 *dt/4;
+	kf_.Q_ = MatrixXd(4, 4);
+	kf_.Q_ << dt4*noise_ax, 0, dt3*noise_ax, 0,
+			  0, dt4*noise_ay, 0, dt3*noise_ay,
+			  dt3*noise_ax, 0, dt2*noise_ax, 0,
+			  0, dt3*noise_ay, 0, dt2*noise_ay;
+
+	//3. Call the Kalman Filter predict() function
+	kf_.Predict();
+	//4. Call the Kalman Filter update() function
+	kf_.Update(measurement_pack.raw_measurements_);
+	// with the most recent raw measurements_
+	
+	std::cout << "x_= " << kf_.x_ << std::endl;
+	std::cout << "P_= " << kf_.P_ << std::endl;
+
+}
+
+
